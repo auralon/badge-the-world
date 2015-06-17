09ea5ae191dac90daa864e46d773d78bebb62b47
Keith Hill <keith@bluemantis.com>
2015-06-17 11:14:54 +0100

    Added footer links, reversed twitter feed order, adjusted contact style and added European countries only filter option

--- routes/index.js
+++ routes/index.js
@@ -166,7 +166,7 @@ router.post('/createPledge', csrfProtection, function(req, res) {
 			sendPledgeEmail(res, pledge, function(err, res) {
 				res.setHeader('Content-Type', 'application/json');
 				if (err) {
-					return res.send({'status':'error'});
+					console.log(err);
 				}
 				return res.send({'status':'success', 'pledge': pledge});
 			});
@@ -263,24 +263,24 @@ router.post('/deletePledge', csrfProtection, function(req, res) {
 /*
  REGISTER USER PAGE
  */
-// router.get('/register', function(req, res) {
-// 	res.render('register', { });
-// });
+router.get('/register', function(req, res) {
+	res.render('register', { });
+});
 
 /*
  REGISTER USER SUBMISSION
  */
-// router.post('/register', function(req, res) {
-// 	Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-// 		if (err) {
-// 			return res.render('register', { account : account });
-// 		}
+router.post('/register', function(req, res) {
+	Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+		if (err) {
+			return res.render('register', { account : account });
+		}
 
-// 		passport.authenticate('local')(req, res, function () {
-// 			res.redirect('/');
-// 		});
-// 	});
-// });
+		passport.authenticate('local')(req, res, function () {
+			res.redirect('/');
+		});
+	});
+});
 
 /*
  LOGIN PAGE
