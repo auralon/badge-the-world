d7db641010cad61981cba1547da4a6e067045a3f
Keith Hill <keith@bluemantis.com>
2015-06-24 12:50:57 +0100

    Fixed bug in admin section

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 	var pledgeUrl = "/getPledges";
 	$.getJSON(pledgeUrl, function(data) {
 
+		console.log(data)
+
 		for (var i = 0; i <=  data.length -1; i++) {
 
 			var row = '<tr>'
@@ -14,7 +16,6 @@ $(document).ready(function() {
 					+ '<td>' + data[i].name + '</td>'
 					+ '<td>' + data[i].twitterHandle + '</td>'
 					+ '<td>' + data[i].organisation + '</td>'
-					+ '<td><ul><li>' + data[i].share.split(", ").join('</li><li>') + '</li></ul></td>'
 					+ '<td>' + new Date(data[i].created_at).toISOString().substr(0, 19).replace('T', ' ') + '</td>'
 					+ '<td><a href="/update?pledge=' + data[i].uid + '">Edit</a>&nbsp;<a href="#" data-id="' + data[i].uid + '" class="delete">Delete</a>'
 					+ '</tr>'
