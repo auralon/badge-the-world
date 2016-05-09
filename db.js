0783f367e7820ce7047d09d6e824042357402dc4
Keith Hill <keith@bluemantis.com>
2016-05-09 11:57:22 +0100

    Removed sensitive (and since expired) database connection details

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 var pg = require('pg'),
 	hstore = require('pg-hstore')(),
 	Sequelize = require('sequelize'),
-	rdb = (process.env.DATABASE_URL !== undefined) ? process.env.DATABASE_URL : 'postgres://ocsarfjdptkzwi:Dtsh8MiBk8vRF0QRiJbSgZvv9u@ec2-54-217-245-173.eu-west-1.compute.amazonaws.com:5432/di979em6cd5uf',
+	rdb = (process.env.DATABASE_URL !== undefined) ? process.env.DATABASE_URL : 'postgres://postgres:password@localhost:5432/postgres',
 	match = rdb.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
 	sequelize = new Sequelize(match[5], match[1], match[2], {
-	    dialect:  'postgres',
-	    protocol: 'postgres',
-	    port:     match[4],
-	    host:     match[3],
-	    logging: false,
-	    dialectOptions: {
-	        ssl: true
-	    }
+		dialect:  'postgres',
+		protocol: 'postgres',
+		port:     match[4],
+		host:     match[3],
+		logging: false,
+		dialectOptions: {
+			ssl: true
+		}
 	});
 
 module.exports = {
